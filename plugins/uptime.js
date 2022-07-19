a3505fd4726add9b0ef4a31c8ea1c47e3497e8a3
Ziv San <106360050+Zivfurr@users.noreply.github.com>
2022-07-19 16:34:05 +0700

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -1,51 +1,16 @@
-let moment = require('moment-timezone')
-let fs = require('fs')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command }) => {
-//let img1 = fs.readFileSync('./src/img1.png')
-//let img2 = fs.readFileSync('./src/img2.png')
-let user = global.DATABASE.data.users[m.sender]
-/*conn.sendFile(m.chat, img, '', `Halo juga kak ${conn.getName(m.sender)}`, { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 999, thumbnail: fs.readFileSync('./src/mikey.jpg')
-}}})*/
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = m.fromMe ? conn.user : conn.contacts[who]
-pushname2 = `*${name.vnmae || name.notify || name.name || ('+' + name.jid.split`@`[0])}*`
+let handler = async (m, { usedPrefix, command }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let fetch = require("node-fetch")
+let who = m.sender
+let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
+let runnya = `
+*───「 RUNTIME BOT 」───*
 
-function kyun(seconds){
-function pad(s){
-return (s < 10 ? '0' : '') + s;
-}
-var hours = Math.floor(seconds / (60*60));
-var minutes = Math.floor(seconds % (60*60) / 60);
-var seconds = Math.floor(seconds % 60);
-
-//return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
-return `${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik`
-}
-runtime = process.uptime()
-					teks = `The.sad.boy01 ×͜×`// \n\n${kyun(runtime)}`
-					run = `${kyun(runtime)}`
-					var itsme = `0@s.whatsapp.net`
-					var split = `teks`
-					const ftrol = {
-	key : {
-                          participant : '0@s.whatsapp.net'
-                        },
-       message: {
-                    orderMessage: {
-                            itemCount : 999,
-                            status: 1,
-                            surface : 1,
-                            message: `${teks}\n${run}`, //Kasih namalu
-                            orderTitle: `${teks}\n${run}`,
-                            thumbnail: bg, //Gambarnye
-                            sellerJid: '0@s.whatsapp.net' 
-                          }
-                        }
-                      }
-					//m.reply(teks)
-					let jawab = `*Bot aktif selama*\n${run}\n\n` + wm .trim()
-conn.reply(m.chat, jawab, wm, {quoted: ftrol})
+⏲️ Waktu: ${time} WIB
+💌 Aktif Selama : ${uptime}
+🌹 Tag : @${who.split`@`[0]}`
+conn.sendButton(m.chat, runnya, wm, 'Menu', '.menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `Simpel Bot Whatsapp`, itemCount: 99999, thumbnail: await (await fetch('https://telegra.ph/file/dd8510a719c854da39483.jpg')).buffer(),}}})
 }
 handler.help = ['runtime']
 handler.tags = ['info']
