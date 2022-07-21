e6edafff161d60664cd1242e2fe5a7493ae1e512
Ziv San <106360050+Zivfurr@users.noreply.github.com>
2022-07-22 06:34:01 +0700

    Update o-broadcastloc.js

--- plugins/o-broadcastloc.js
+++ plugins/o-broadcastloc.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, text }) => {
   let chats = Object.keys(await conn.chats)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
   for (let id of chats) {
-       let bcbg = `${pickRandom(['https://telegra.ph/file/87ed5db99d405cb200742.jpg', 'https://telegra.ph/file/aa1957dc1715f002b85e3.jpg', 'https://telegra.ph/file/57e26fd236cab75d09dbe.jpg'])}` 
+       let bcbg = 'https://telegra.ph/file/9a5e47f91d1707fbb920c.jpg'
        await conn.delay(1500)
        await conn.send2ButtonLoc(id, bcbg,'*——————「 Broadcast 」——————*\n' + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', m)
      }
