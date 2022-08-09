5f156e19aa2446637fd760b19e784a298920235c
Ziv San <106360050+Zivfurr@users.noreply.github.com>
2022-08-09 10:45:40 +0700

    Update jadilokasi.js

--- plugins/jadilokasi.js
+++ plugins/jadilokasi.js
@@ -1,11 +1,19 @@
 let { MessageType } = require('@adiwajshing/baileys')
+const uploadFile = require('../lib/uploadFile')
+const uploadImage = require('../lib/uploadImage')
 let handler = async (m, { conn }) => {
   let q = m.quoted ? m.quoted : m 
   let mime = (q.msg || q).mimetype || ''
   if (/image|video/.test(mime)) {
     let img = await q.download()
     if (!img) throw 'Foto/Sticker tidak ditemukan'
-    conn.sendButtonLoc(m.chat, img, 'Nih Kak', wm, 'Owner', '.owner', m)
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif|webp)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+    conn.sendButtonLoc(m.chat, img, `${link}
+
+${media.length} Byte(s)
+${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`, wm, 'Owner', '.owner', m)
   } else conn.sendButton(m.chat, 'Tag foto/videonya!!', wm, 'Menu', '#Menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: wm, itemCount: 999, thumbnail: await (await fetch('https://telegra.ph/file/deddcdba4caf3c8509295.jpg')).buffer()
 }}})
 }
