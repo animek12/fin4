47b27631c9251473d8c25704430b3b55b43cc0e9
Ziv San <106360050+Zivfurr@users.noreply.github.com>
2022-08-21 13:34:29 +0700

    Update jadilokasi.js

--- plugins/jadilokasi.js
+++ plugins/jadilokasi.js
@@ -10,10 +10,9 @@ let handler = async (m, { conn }) => {
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif|webp)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-    conn.sendButtonLoc(m.chat, img, `${link}
-
-${media.length} Byte(s)
-${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`, wm, 'Owner', '.owner', m)
+    conn.sendButtonLoc(m.chat, await conn.resize(img ,300, 300), `📮 *L I N K :* ${link}
+📊 *S I Z E :* ${media.length} Byte
+📛 *E x p i r e d :* ${isTele ? 'No Expiry Date' : '(Tidak diketahui)'}`, wm, 'Owner', '.owner', m)
   } else conn.sendButton(m.chat, 'Tag foto/videonya!!', wm, 'Menu', '#Menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: wm, itemCount: 999, thumbnail: await (await fetch('https://telegra.ph/file/deddcdba4caf3c8509295.jpg')).buffer()
 }}})
 }
