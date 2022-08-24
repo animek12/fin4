e0ed122de8288c3534ca997afc1e6ca3243216de
Ziv San <106360050+Zivfurr@users.noreply.github.com>
2022-08-24 11:52:13 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -5,6 +5,25 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
   if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
+  let sender = m.sender
+    let pp = await conn.profilePictureUrl(sender, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let ftrol = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: `Nih Audio Nya, Selamat Menikmati 🎧`, 
+    orderTitle: `▮Menu ▸`,
+    thumbnail: await conn.resize(await (await fetch(pp)).buffer(), 300, 300), //Gambarnye
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
@@ -16,9 +35,9 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
 │• *Type:* MP3
 ┆• *📥 Ukuran File:* ${filesizeF}
 └❏
-`.trim(), m, null, {
-    asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: true, contextInfo: {
-        externalAdReply: {
+`.trim(), ftrol, null, {
+    asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: false, contextInfo: {
+        externalAdReply: { showAdAttribution: true,
             title: '▶︎ ━━━━━━━•────────────────── ', 
             body: 'Now Playing...',
             description: 'Now Playing...',
