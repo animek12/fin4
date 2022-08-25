d09fb6b68b24b993e8578f6474324e926b41aa01
Ziv San <106360050+Zivfurr@users.noreply.github.com>
2022-08-25 12:05:58 +0700

    Update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -1,8 +1,14 @@
-let handler = async (m, { conn }) => {
-let totalfeatures = Object.values(global.plugins).filter(
+let handler = async (m, { conn, args, command }) => {
+let sender = m.sender
+let pp = await conn.profilePictureUrl(sender, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/c4a4bea15c41d62072b60.jpg', `*Total Fitur:* ${totalfeatures}`, wm2, 'Info', '.info', m) 
+conn.sendFile(m.chat, 'http://fzcdn.cf/file/dMdcL-HTKpb~wdWBsUM9.webp', 'haori.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true, contextInfo: {
+        externalAdReply: { showAdAttribution: true, title: `Total Fitur Bot Saat Ini: ${totalf}`, body: `${pickRandom(['Follow Tiktok My Bestie'])}`, sourceUrl: 'https://www.tiktok.com/@fory_whitecattiktok?_t=8V5TGON5rgv&_r=1', thumbnail: await (await fetch(pp)).buffer(),}} 
+     })
 }
 
 handler.help = ['totalfitur']
